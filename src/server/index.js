fc598811572bbbd96a7aad4a4e0c66b04073e3c7
Mandy Tao <taoxiumin2016@gmail.com>
2018-06-12 14:18:56 +0800

    Make data identical on both server and client by using window object

---
+++ src/server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from "cors";
+import { renderToString } from 'react-dom/server';
+import App from '../shared/App';
+import React from 'react';
+import serialize from 'serialize-javascript';
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.static("public"))
+
+app.get("*", (req, res, next) => {
+  const name = 'Tyler'
+  const markup = renderToString(
+    <App data={name}/>
+  )
+
+  res.send(`
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title>SSR with RR</title>
+        <script src="/bundle.js" defer></script>
+        <script>window.__INITIAL_DATA__ = ${serialize(name)}</script>
+      </head>
+
+      <body>
+        <div id="app">${markup}</div>
+      </body>
+    </html>
+  `);
+})
+
+app.listen(3000, () => {
+  console.log(`Server is listening on port: 3000`)
+})
