4969e3d7f544172d785025f526ac1c6c7341ff9b
Mandy Tao <taoxiumin2016@gmail.com>
2018-06-12 14:33:10 +0800

    Add Grid for listing popular items.

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Grid from './Grid';
+
 
 class App extends Component {
   render () {
     return (
       <div>
-        Hello World{this.props.data}
+        <Grid data={this.props.data}/>
       </div>
     )
   }
